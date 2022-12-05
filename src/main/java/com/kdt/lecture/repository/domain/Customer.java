a4c65055238d89f1c0ac2f2675ab96fc66b7a4f1
Bosub Kim <grapeflavoredapple@gmail.com>
2022-12-05 15:39:43 +0900

    [feat] mybatis 사용

---
+++ src/main/java/com/kdt/lecture/repository/domain/Customer.java
@@ -0,0 +1,40 @@
+package com.kdt.lecture.repository.domain;
+
+import org.apache.ibatis.type.Alias;
+
+@Alias("customers")
+public class Customer {
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    public Customer(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/test/java/com/kdt/lecture/MyBatisTest.java
@@ -0,0 +1,33 @@
+package com.kdt.lecture;
+
+import com.kdt.lecture.repository.CustomerMapper;
+import com.kdt.lecture.repository.domain.Customer;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Slf4j
+@SpringBootTest
+public class MyBatisTest {
+    static final String DROP_TABLE_SQL = "drop table customers if exists";
+    static final String CREATE_TABLE_SQL = "CREATE TABLE customers(id SERIAL, first_name varchar(255), last_name varchar(255))";
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;;
+
+    @Autowired
+    CustomerMapper customerMapper;
+
+    @Test
+    void save_test(){
+        jdbcTemplate.update(DROP_TABLE_SQL);
+        jdbcTemplate.update(CREATE_TABLE_SQL);
+
+        customerMapper.save(new Customer(1L, "bosub", "kim"));
+        Customer customer = customerMapper.findById(1L);
+
+        log.info("full Name : {}, {}", customer.getFirstName(), customer.getLastName());
+    }
+}
